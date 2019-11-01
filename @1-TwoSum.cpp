de8fd60a712e5ba51d7aea41bc105f82bf6742b8
gloria-kim <51448370+gloria-kim@users.noreply.github.com>
2019-11-01 04:35:06 -0500

    Update @1-TwoSum.cpp

--- @1-TwoSum.cpp
+++ @1-TwoSum.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+   
         int sol = 0;
         
-        for (int i = 0; i < nums.size(); i++)
-        {
-            for (int j = 0; j < nums.size(); j++)
-            {
-                sol = nums[i]+nums[j]
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < nums.size(); j++) {
+                sol = nums[i]+nums[j];
                 
-                if (sol == target)
-                {
-                    return [i, j];
-                }   
-                    
+                if (sol == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return nums;
+                }          
             }
-        }           
+        }
     }
+    return NULL;
 };
